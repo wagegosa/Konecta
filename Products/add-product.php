f694a4be56f5220990d08174bf8c8550c016bc9a
Gerardo González <wagegosa@gmail.com>
2020-06-30 11:13:44 -0500

    Se modifica ya que tenia un error al momento presentaba un error ya que el id estaba nullo y este nunca se envía.

--- Products/add-product.php
+++ Products/add-product.php
@@ -20,7 +20,11 @@ if(isset($data->NombreProducto) && isset($data->Referencia) && isset($data->prec
     $Categoria = mysqli_real_escape_string($db_conn, trim($data->Categoria));
     $Stock = mysqli_real_escape_string($db_conn, trim($data->Stock));
     $FechaCreacion = mysqli_real_escape_string($db_conn, trim($data->FechaCreacion));
-    $insertProduct = mysqli_query($db_conn,"productos(ID, NombreProducto, Referencia, precio, Peso, Categoria, Stock, FechaCreacion) VALUES('$ID, $NombreProducto, $Referencia, $precio, $Peso, $Categoria, $Stock, $FechaCreacion')");
+    $insertProduct = mysqli_query($db_conn,"INSERT INTO inventario.productos(NombreProducto, Referencia, precio, Peso, Categoria, Stock, FechaCreacion) VALUES('$NombreProducto', '$Referencia', $precio, $Peso, '$Categoria', '$Stock', '$FechaCreacion')");
+    /*echo "<pre>";
+    echo "INSERT INTO inventario.productos(NombreProducto, Referencia, precio, Peso, Categoria, Stock, FechaCreacion) VALUES('$NombreProducto', '$Referencia', $precio, $Peso, '$Categoria', '$Stock', '$FechaCreacion')";
+    echo "</pre>";
+    die;*/
     if($insertProduct){
         $last_id = mysqli_insert_id($db_conn);
         echo json_encode(["success"=>1,"msg"=>"Productos insertado.","id"=>$last_id]);
@@ -32,4 +36,3 @@ if(isset($data->NombreProducto) && isset($data->Referencia) && isset($data->prec
 else{
     echo json_encode(["success"=>0,"msg"=>"¡Porfavor ingrese los datos ingresado!"]);
 }
-?>
