b7120c4bc38b1d00df5771c55a51be4ce4d4d0f1
Gerardo González <wagegosa@gmail.com>
2020-07-03 12:05:55 -0500

    Se modifica el ID ya que este no mostraba la consulta de la DB y se agrega clases para la tabla.

--- php-react-crud/src/components/GetProducts.js
+++ php-react-crud/src/components/GetProducts.js
@@ -6,59 +6,56 @@ class GetProducts extends Component {
 	componentDidMount() {
 		this.context.get_products();
 	}
-
 	handleUpdate = (id) => {
-		//this.context.handleUpdate(id, this.NombreProducto.value, this.Referencia.value, this.precio.value, this.Peso.value, this.Categoria.value, this.Stock.value, this.FechaCreacion.value);
 		this.context.handleUpdate(id, this.NombreProducto.value, this.Referencia.value, this.precio.value, this.Peso.value, this.Categoria.value, this.Stock.value, this.FechaCreacion.value);
 	}
-
 	render() {
 		let allProducts;
 		let mainData;
-		allProducts = this.context.all_products.map(({ id, NombreProducto, Referencia, precio, Peso, Categoria, Stock, FechaCreacion, isEditing }) => {
+		allProducts = this.context.all_products.map(({ ID, NombreProducto, Referencia, precio, Peso, Categoria, Stock, FechaCreacion, isEditing }) => {
 			return isEditing === true ? (
-				<tr key={id}>
+				<tr key={ID}>
 					<td><input className="form-control" type="text" ref={(item) => this.NombreProducto = item} defaultValue={NombreProducto} /></td>
 					<td><input className="form-control" type="text" ref={(item) => this.Referencia = item} defaultValue={Referencia} /></td>
 					<td><input className="form-control" type="text" ref={(item) => this.precio = item} defaultValue={precio} /></td>
 					<td><input className="form-control" type="text" ref={(item) => this.Peso = item} defaultValue={Peso} /></td>
 					<td><input className="form-control" type="text" ref={(item) => this.Categoria = item} defaultValue={Categoria} /></td>
 					<td><input className="form-control" type="text" ref={(item) => this.Stock = item} defaultValue={Stock} /></td>
-					<td><input className="form-control" type="text" ref={(item) => this.FechaCreacion = item} defaultValue={FechaCreacion} /></td>
+					<td><input className="form-control" type="date" ref={(item) => this.FechaCreacion = item} defaultValue={FechaCreacion} /></td>
 					<td>
-						<button className="btn btn-success mr-2" onClick={() => this.handleUpdate(id)}>Guardar</button>
-						<button onClick={() => this.context.cancelEdit(id)} className="btn btn-light">Cancelar</button>
+						<button className="btn btn-success mr-2" onClick={() => this.handleUpdate(ID)}>Actulizar</button>
+						<button onClick={() => this.context.cancelEdit(ID)} className="btn btn-light">Cancelar</button>
 					</td>
 				</tr>
 			) : (
-				<tr key={id}>
-					<td>{NombreProducto}</td>
-					<td>{Referencia}</td>
-					<td>{precio}</td>
-					<td>{Peso}</td>
-					<td>{Categoria}</td>
-					<td>{Stock}</td>
-					<td>{FechaCreacion}</td>
-					<td>
-						<button className="btn btn-dark mr-2" onClick={() => this.context.editMode(id)}>Edit</button>
-						<button onClick={() => this.context.handleDelete(id)} className="btn btn-danger">Delete</button>
-					</td>
-				</tr>
-			);
+					<tr key={ID}>
+						<td>{NombreProducto}</td>
+						<td>{Referencia}</td>
+						<td>{precio}</td>
+						<td>{Peso}</td>
+						<td>{Categoria}</td>
+						<td>{Stock}</td>
+						<td>{FechaCreacion}</td>
+						<td>
+							<button className="btn btn-dark mr-2" onClick={() => this.context.editMode(ID)}>Edit</button>
+							<button onClick={() => this.context.handleDelete(ID)} className="btn btn-danger">Delete</button>
+						</td>
+					</tr>
+				);
 		});
 		if (this.context.all_products.length > 0) {
 			mainData = (
-				<table className="table table-striped table-bordered">
+				<table className="table table-bordered  table-hover table-striped text-center" width="100%">
 					<thead>
 						<tr>
-							<th>Nombre Producto</th>
-							<th>Referencia</th>
-							<th>precio</th>
-							<th>Peso</th>
-							<th>Categoria</th>
-							<th>Stock</th>
-							<th>FechaCreacion</th>
-							<th>Actions</th>
+							<th data-filterable="false">Nombre Producto</th>
+							<th data-breakpoints="xs sm" >Referencia</th>
+							<th data-breakpoints="xs sm" >precio</th>
+							<th data-breakpoints="xs sm" >Peso</th>
+							<th data-breakpoints="xs sm" >Categoria</th>
+							<th data-breakpoints="xs sm" >Stock</th>
+							<th data-breakpoints="xs sm" >FechaCreacion</th>
+							<th data-breakpoints="xs sm" data-filterable="false">Actions</th>
 						</tr>
 					</thead>
 					<tbody>
