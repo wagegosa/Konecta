c5f6727be8ed3627848cbefb123aa320d312f10e
Gerardo González <wagegosa@gmail.com>
2020-07-03 11:58:40 -0500

    Se quita comentario y espacios.

--- Products/update-product.php
+++ Products/update-product.php
@@ -8,7 +8,6 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 require 'db_connection.php';
 
 $data = json_decode(file_get_contents("php://input"));
-
 if(isset($data->id) && isset($data->NombreProducto) && isset($data->Referencia) && isset($data->precio) && isset($data->Peso) && isset($data->Categoria) && isset($data->Stock) && isset($data->FechaCreacion)
 	&& is_numeric($data->id) && !empty(trim($data->NombreProducto)) && !empty(trim($data->Referencia)) && !empty(trim($data->precio)) && !empty(trim($data->Peso)) && !empty(trim($data->Categoria)) && !empty(trim($data->Stock)) && !empty(trim($data->FechaCreacion)) 
 ){
@@ -19,7 +18,11 @@ if(isset($data->id) && isset($data->NombreProducto) && isset($data->Referencia) 
     $Categoria = mysqli_real_escape_string($db_conn, trim($data->Categoria));
     $Stock = mysqli_real_escape_string($db_conn, trim($data->Stock));
     $FechaCreacion = mysqli_real_escape_string($db_conn, trim($data->FechaCreacion));
-    $updateProduct = mysqli_query($db_conn,"UPDATE productos SET NombreProducto=$NombreProducto,Referencia=$Referencia,precio=$precio,Peso=$Peso,Categoria=$Categoria,Stock=$Stock,FechaCreacion=$FechaCreacion WHERE WHERE id='$data->id'");
+    $updateProduct = mysqli_query($db_conn,"UPDATE productos SET NombreProducto= '$NombreProducto', Referencia= '$Referencia', precio= $precio, Peso= $Peso, Categoria='$Categoria', Stock= '$Stock', FechaCreacion= '$FechaCreacion' WHERE id=$data->id");
+    /*echo "<pre>";
+    print_r("UPDATE productos SET NombreProducto= '$NombreProducto', Referencia= '$Referencia', precio= $precio, Peso= $Peso, Categoria='$Categoria', Stock= '$Stock', FechaCreacion= '$FechaCreacion' WHERE id=$data->id");
+    echo "</pre>";
+    die;*/
     if($updateProduct){
         echo json_encode(["success"=>1,"msg"=>"Producto actualizado."]);
     }
